0441b70fea9840f1d48fdbdf81d3c391734944c2
Stephen Johnson <stephenjohnson@whitworth.edu>
2016-09-13 15:05:42 -0700

    Initial Commit

---
+++ Hw1/Hw1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Hw1
+//
+//  Created by Stephen Johnson on 9/13/16.
+//  Copyright © 2016 Glacier Peak Studios LLC. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World! Im in CS 172\n";
+    std::cout << "Here is another line\n";
+    std::cout << "Hello our names are Stephen and Eben";
+
+    return 0;
+}
