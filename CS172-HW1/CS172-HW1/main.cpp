a321ebad2ca298d55251c70f88fb343c25841e9e
StephenLJohnson <stephen@glacierPeakStudios.com>
2016-09-13 17:03:28 -0700

    Don't Forget the message

--- CS172-HW1/CS172-HW1/main.cpp
+++ CS172-HW1/CS172-HW1/main.cpp
@@ -12,5 +12,6 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
     std::cout << "Here is another line\n";
+	std::cout << "Here is another line again\n";
     return 0;
 }
